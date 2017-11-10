68418e91ff3bb2433e3d21185ea65ec4bd72ab30
David Arango <davidarango@Davids-MacBook-Pro.local>
2017-11-09 20:33:15 -0500

    Added Nasa APOD Api as well as some styling

--- js/main.js
+++ js/main.js
@@ -1,3 +1,49 @@
+////
+// Author : David Arango
+///
+$(document).ready(() => {
+	// Api variables from NASA and NY Times
+	const nasaUrl = 'https://api.nasa.gov/planetary/apod?api_key=';
+	const nasaUrlTwo = 'http://crossorigin.me/https://api.nasa.gov/techport/api/items/10856.json?api_key=';
+	const apiKey = 'yt7WhJOrDXyKi63Q7jMDj8A59xyWfTgfdBqW11m8';
+	// Elements where Api JSON objects will be appended to
+	const picOfDay = $('#pic-of-day');
+	const picOfMars = $('#pic-of-mars');
+	const picOfDayButton = $('#button-one');
+	const picOfMarsButton = $('#button-two');
+	// Call back function returns a promise of a nasa JSON object
+	async function getData() {
+		try {
+			let response = await fetch(nasaUrl + apiKey);
+			if(response.ok) {
+				let jsonResponse = await response.json();
+				picOfDay.css('display', 'block');
+				picOfDay.append(`<img class='picOfDay-active' src='${jsonResponse.hdurl}'/>`);
+				console.log(jsonResponse);
+			}
+
+		}catch(error){
+			console.log(error);
+		}
+	};
+	// Call back function returns a promise of a NY Times JSON object
+	async function getDataTwo() {
+		try {
+			let response = await fetch(nasaUrlTwo + apiKey);
+			if(response.ok) {
+				let jsonResponse = await response.json();
+				console.log(jsonResponse);
+			}
+		}catch(error) {
+			console.log(error);
+		}
+	};
+	// Event Listeners
+	picOfDayButton.on('click', getData);
+	picOfMarsButton.on('click', getDataTwo);
+
+});
+
 // Parent class for spaceships
 class SpaceCraft {
 	constructor(model, capacity, price) {
@@ -53,7 +99,7 @@ class SpaceCraft {
 		<li class = 'space_craft_info_list'> Ticket Price: $${this._price} </li>
 		<li class = 'space_craft_info_list'> NOTE: Children are 75% off, and Elderly are 15% off</li> `;
 	}
-}
+};
 // Child class 
 class SpaceCraftOne extends SpaceCraft {
 	constructor(model, capacity, price) {
@@ -61,14 +107,14 @@ class SpaceCraftOne extends SpaceCraft {
 		this._speed = 24000;
 		this._timeToMars = '400 Days';
 	}
-}
+};
 // Child class 
 class SpaceCraftTwo extends SpaceCraft {
 	constructor(model, capacity, price) {
 		super(model, capacity, price);
 		this._price = 5000;
 	}
-}
+};
 // Child class 
 class SpaceCraftThree extends SpaceCraft {
 	constructor(model, capacity, price) {
@@ -77,7 +123,7 @@ class SpaceCraftThree extends SpaceCraft {
 		this._timeToMars = '1 Day';
 		this._price = 10000;
 	}
-}
+};
 // New child class instances
 const ApolloEleven = new SpaceCraftOne('Apollo 11', 15);
 // New child class instances
@@ -89,17 +135,45 @@ const spaceCraftInfo = [ApolloEleven, SpaceShuttle, Ufo];
 // Array of aircraft images to cycle through selection
 const spaceCraft = ['img/apolloEleven.svg', 'img/spaceShuttle.svg', 'img/ufo.svg'];
 // Right button when on click changes vehicle and stats
-const rightButton = document.getElementById('right_selection');
+const rightButton = document.getElementById('right-selection');
 // Left button, when on click changes vehicle and stats
-const leftButton = document.getElementById('left_selection');
+const leftButton = document.getElementById('left-selection');
 // Where the rocket info stats will be appended to
-const rocketInfo = document.getElementById('rocket_info');
+const rocketInfo = document.getElementById('rocket-info');
 // Where the rocket seat stats will populated
-const rocketSeats = document.getElementById('rocket_seats');
+const rocketSeats = document.getElementById('rocket-seats');
 // spaceCraft will be appended to this section
 const vehicle = document.getElementById('vehicle');
-
+// Ticket price total will be shown here
 const total = document.getElementById('total');
+// This button will execute a callback function and append ticket information 
+const submitButton = document.getElementById('submit');
+// This button will activate the take off animation into space
+const buyButton = document.getElementById('purchase-button');
+// Grabbing this element to change background into a space background
+const vehicleBackground = document.getElementById('vehicle-container'); 
+// Grabbed this element in order to change display property to none when user clicks the 'buy' button
+const landingPad = document.getElementById('landing-pad');
+// Grabbed this element in order to change display property to none when user clicks the 'buy' button
+const buttonContainer =  document.getElementById('button-container');
+// This button opens up the news menu located below the spaceCraft on the app
+const openNewsMenuButton = document.getElementById('news-menu');
+// This is the element whose class I am adding and removing to add transition affect
+const newsMenuContainer = document.getElementById('nasa-news-container');
+// This element listens for closing function to close news menu
+const closeNewsMenuButton = document.getElementById('close-news-menu');
+// This button listens for function that opens the stats menu
+const openStatsButton = document.getElementById('open-stats');
+// Grabbed this element to append and remove class that allows transition affect
+const closeStatsButton = document.getElementById('close-stats');
+
+const statsContainer = document.getElementById('stats');
+
+const openTicketButton = document.getElementById('open-tickets');
+
+const closeTicketButton = document.getElementById('close-tickets');
+
+const ticketContainer = document.getElementById('right-container');
 
 let infoCounter = -1;
 let spaceCraftCounter = -1;
@@ -127,7 +201,7 @@ function selectVehicleRight() {
 			vehicle.src = `${spaceCraft[spaceCraftCounter]}`;
 		}
 	});
-}
+};
 // Function below cycles through both child instances and spacecraft arrays and appends to the DOM, the selected vehicle of choice
 function selectVehicleLeft() {
 	infoCounter--;
@@ -152,9 +226,8 @@ function selectVehicleLeft() {
 			vehicle.src = ` ${spaceCraft[spaceCraftCounter]} `;
 		}
 	});
-}
-rightButton.addEventListener('click', selectVehicleRight);
-leftButton.addEventListener('click', selectVehicleLeft);
+};
+// New Ticket class for calculating ticket prices
 class Ticket {
 	constructor(children, adults, seniors, pets) {
 		this._children = children;
@@ -174,7 +247,7 @@ class Ticket {
 	get pets() {
 		return this._pets;
 	}
-
+	//File/Form Handler fucntion has a conditional which checks to see what aircraft should be assigned what price
 	ticketPrice(numOfChild, numOfAdult, numOfSenior, numOfPet) {
 		let type = 0;
 		if(vehicle.src === 'file:///Users/davidarango/Desktop/my-work/ticket-to-mars/img/apolloEleven.svg') {
@@ -190,6 +263,7 @@ class Ticket {
 			console.log(vehicle.src);
 		}
 	}
+	// Estimates and appends ticket prices
 	print(numOfChild, numOfAdult, numOfSenior, numOfPet, type) {
 		//Apollo variables
 		// Child price variables
@@ -210,9 +284,9 @@ class Ticket {
 		const seniorTotal = seniorSum * numOfSenior;
 		// Pet price variables
 		const petTotal = type * numOfPet;
-
+		// Price variables total
 		const grandTotal = childTotal + adultTotal + petTotal;
-
+		// This appends calculations based on user input of passenger counts
 			total.innerHTML = `
 				<li class="total_price_info"> Children(${numOfChild}): $${childTotal} - You Saved $${childTotalSaved}! </li>
 				<li class="total_price_info"> Adults(${numOfAdult}): $${adultTotal}</li>
@@ -221,34 +295,70 @@ class Ticket {
 				<li class="total_price_info"> ${numOfChild} Children, ${numOfAdult} Adults, ${numOfSenior} Seniors, ${numOfPet} Pets</li>
 				<li class="total_price_info"> Grand Total: $${grandTotal}</li>`;
 	}
-}
-const submitButton = document.getElementById('submit');
-submitButton.addEventListener('click', () => {
+};
+// Call back function to print ticket price on screen
+const printTicket = () => {
 	const numOfChild = document.getElementById('child').value;
 	const numOfAdult = document.getElementById('adult').value;
 	const numOfSenior = document.getElementById('senior').value;
 	const numOfPet = document.getElementById('pet').value;
 	const ticket = new Ticket();
 	ticket.ticketPrice(numOfChild, numOfAdult, numOfSenior, numOfPet);	
-});
+};
+// Call back function to animate spaceCraft upon buying ticket
+const toMars = () => {
+	ticketContainer.className = 'right_container';
+	vehicle.className = 'space_craft space_craft_takeOff';
+	const spaceLaunch = setTimeout(() => {
+		if(vehicle.style.visibility === 'visible') {
+			buttonContainer.style.display = 'none';
+			landingPad.style.visibility = 'hidden';
+			vehicleBackground.style.height = '100%';
+			vehicleBackground.style.background = `black url('file:///Users/davidarango/Desktop/my-work/ticket-to-mars/img/space.gif') no-repeat center`;
+			vehicle.style.transition = 'unset';
+			vehicle.className = 'space_craft';
+		}else {
+			alert('Please choose a SpaceCraft');
+		}
+	}, 1700);
+};
+// Call back function which slides the news menu up
+const newsSlideUp = () => {
+	newsMenuContainer.className = 'nasa_news_container nasa-news-container-active';
+};
+// Call back function which slides down the news menu
+const newsClose = () => {
+	newsMenuContainer.className = 'nasa_news_container';
+};
 
-// const myApi = 'CvpD6vPiCFT6eyc71AaRtkBtXMga9ujqZq6fgwtb';
-// const techPortUrl = 'https://api.nasa.gov/techport/api/items/{id_parameter}';
-// $(document).ready(() => {
-// 	async function getData() {
-// 		const rocketsUrl = 'https://api.spacexdata.com/v1/vehicles';
-// 		try {
-// 			let response = await fetch(rocketsUrl);
-// 			if(response.ok) {
-// 				const jsonResponse = await response.json();
-// 				const rocket = jsonResponse[0];
-// 				$('#rocket').append(JSON.stringify(rocket));
-// 				console.log(jsonResponse, rocket);
-// 			}
-// 			// throw new Error('Request failed!');
-// 		}catch(error) {
-// 			console.log(error);
-// 		}
-// 	}
-// 	$('#ticket_button').on('click', getData);
-// });
+const statsOpen = () => {
+	statsContainer.className = 'left_container left_container-active';
+	console.log(statsContainer.className);
+};
+
+const statsClose = () => {
+	statsContainer.className = 'left_container';
+};
+
+const openTickets = () => {
+	ticketContainer.className = 'right_container right_container-active';
+	console.log(ticketContainer);
+};
+
+const closeTickets = () => {
+	ticketContainer.className = 'right_container';
+};
+
+// Event Listeners
+rightButton.addEventListener('click', selectVehicleRight);
+leftButton.addEventListener('click', selectVehicleLeft);
+submitButton.addEventListener('click', printTicket);
+buyButton.addEventListener('click', toMars);
+openNewsMenuButton.addEventListener('click', newsSlideUp);
+closeNewsMenuButton.addEventListener ('click', newsClose);
+openStatsButton.addEventListener('click', statsOpen);
+closeStatsButton.addEventListener('click', statsClose);
+openTicketButton.addEventListener('click', openTickets);
+closeTicketButton.addEventListener('click', closeTickets);
+
+
